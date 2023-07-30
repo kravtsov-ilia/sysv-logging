48b8583d5d9bf42474ee011682ec5ebe1cad866d
Ilia Kravtsov <kravcov2512@gmail.com>
2023-07-30 18:29:35 +0400

    init

---
+++ handler.py
@@ -0,0 +1,24 @@
+import json
+import logging
+import sysv_ipc
+
+
+class SysvIPCHandler(logging.Handler):
+    def __init__(self, *, queue_key: int, max_message_size: int, level: int = logging.INFO):
+        super().__init__(level)
+        self.messages_queue = sysv_ipc.MessageQueue(
+            key=queue_key,
+            flags=sysv_ipc.IPC_CREAT,
+            max_message_size=max_message_size,
+        )
+
+    def prepare_log_data(self) -> dict:
+        raise NotImplementedError()
+
+    def emit(self, record):
+        self.acquire()
+        try:
+            msg = self.prepare_log_data()
+            self.messages_queue.send(json.dumps(msg).encode('utf-8'))
+        finally:
+            self.release()
